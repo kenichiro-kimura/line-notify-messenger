ac8d09d9f53120380fc6317ab950a27d481c188d
Kenichiro Kimura <39895593+kenichiro-kimura@users.noreply.github.com>
2025-02-13 13:51:31 +0900

    [add][modify] s3バケットを作るスタックを追加、併せてREADMEを修正

--- bin/line-notify-messenger.ts
+++ bin/line-notify-messenger.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { LambdaStack } from '../lib/lambda-stack';
+import { S3Stack } from '../lib/s3-stack';
 
 const app = new cdk.App();
 new LambdaStack(app, 'LambdaStack');
+new S3Stack(app, 'S3Stack');
--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // S3バケットの参照
+    const bucket = Bucket.fromBucketName(this, 'LineNotifyMessengerImageBucket', 'line-notify-messenger-image-bucket-name');
+
     // Lambda関数の作成
     const myFunction = new lambda.Function(this, 'LineNotifyMessenger', {
       runtime: lambda.Runtime.NODEJS_22_X,
       handler: 'handler.handler',
-      code: lambda.Code.fromAsset('dist')
+      code: lambda.Code.fromAsset('dist'),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+        LINE_CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN || '',
+      },
     });
 
     // Lambda Function URL の作成
