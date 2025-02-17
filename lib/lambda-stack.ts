8abf6b7d7729e6c69544cc64eff471de5d67f864
Kenichiro Kimura <39895593+kenichiro-kimura@users.noreply.github.com>
2025-02-17 20:16:18 +0900

    [modify] handler -> lambdahandler

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,14 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { IBucket } from 'aws-cdk-lib/aws-s3';
+
+export interface LambdaStackProps extends cdk.StackProps {
+  bucket: IBucket;
+}
 
 export class LambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
 
-    // S3バケットの参照
-    const bucket = Bucket.fromBucketName(this, 'LineNotifyMessengerImageBucket', 'line-notify-messenger-image-bucket-name');
+    // S3バケットは S3Stack で作成されたものを受け取る
+    const bucket = props.bucket;
 
     // Lambda関数の作成
     const myFunction = new lambda.Function(this, 'LineNotifyMessenger', {
@@ -18,8 +22,14 @@ export class LambdaStack extends cdk.Stack {
       environment: {
         BUCKET_NAME: bucket.bucketName,
         LINE_CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN || '',
+        AUTHORIZATION_TOKEN: process.env.AUTHORIZATION_TOKEN || '',
+        S3_REGION: bucket.env.region,
       },
+      timeout: cdk.Duration.minutes(3),
     });
+
+    // S3バケットへの読み書き権限を付与
+    bucket.grantReadWrite(myFunction);
 
     // Lambda Function URL の作成
     const functionUrl = myFunction.addFunctionUrl({
