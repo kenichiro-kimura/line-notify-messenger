abfa163aba6e479255939909a3c066bce7daa4b3
Kenichiro Kimura <39895593+kenichiro-kimura@users.noreply.github.com>
2025-02-25 19:03:10 +0900

    [modify] lambdahandler.tsからビジネスロジックを分離

--- src/lambdahandler.ts
+++ src/lambdahandler.ts
@@ -1,92 +1,28 @@
-import { parse } from 'querystring';
-import LineService from './lineService';
 import { S3ImageStorage } from './s3ImageStorage';
 import { JimpImageConverter } from './jimpImageConverter';
+import { LambdaLineNotifyMessenger } from './lambdaLineNotifyMessenger';
+import { LineNotifyMessengerApp } from './lineNotifyMessengerApp';
 
 const multipart = require('aws-lambda-multipart-parser');
-
-const httpUnAuthorizedErrorMessage = (message: string) => {
-    return {
-        statusCode: 401,
-        body: JSON.stringify({ message: message }),
-    };
-};
-
-const httpInternalServerErrorMessage = (message: string) => {
-    return {
-        statusCode: 500,
-        body: JSON.stringify({ message:message }),
-    };
-};
-
-const httpOkMessage = (message: string) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: message }),
-    };
-};
-
-const isNotifyServiceRequest = (path: string,method: string, contentType: string): boolean => {
-    if(path === '/notify' && method === 'POST' && ( contentType === 'application/x-www-form-urlencoded' || contentType.startsWith('multipart/form-data'))) {
-        return true;
-    }
-    return false;
-};
-
-const sendBroadcastMessage = async (lineService: LineService,formData: any) => {
-    await lineService.broadcastMessage(formData);
-};
-
-const replyDefaultMessage = async (lineService: LineService, body: any) => {
-    const replyToken = body.events[0].replyToken;
-    await lineService.replyMessage(replyToken, 'お送り頂いたメッセージはどこにも送られないのでご注意ください');
-};
 
 export const handler = async (event: any) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
 
+    const messenger = new LambdaLineNotifyMessenger(event);
+
     const lineChannelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN;
     if (!lineChannelAccessToken) {
-        return httpInternalServerErrorMessage('LINE_CHANNEL_ACCESS_TOKEN is not set');
+        return messenger.httpInternalServerErrorMessage('LINE_CHANNEL_ACCESS_TOKEN is not set');
     }
 
-    /* notify event */
-    const contentType = event.headers?.['content-type'] || event.headers?.['Content-Type'];
     const bucketName = process.env.BUCKET_NAME;
     const s3Region = process.env.S3_REGION;
 
     if (!bucketName || !s3Region) {
-        return httpInternalServerErrorMessage('BUCKET_NAME or S3_REGION is not set');
+        return messenger.httpInternalServerErrorMessage('BUCKET_NAME or S3_REGION is not set');
     }
 
-    const lineService = new LineService(lineChannelAccessToken, new S3ImageStorage(bucketName, s3Region), new JimpImageConverter());
+    const app = new LineNotifyMessengerApp(messenger, lineChannelAccessToken, new S3ImageStorage(bucketName, s3Region), new JimpImageConverter());
 
-    if (event.isBase64Encoded === true){
-        event.body = Buffer.from(event.body, 'base64').toString('binary');
-    }
-
-    if(isNotifyServiceRequest(event.rawPath, event.requestContext?.http?.method, contentType)) {
-        const bearerToken = event.headers?.authorization?.split('Bearer ')[1];
-
-        if (!bearerToken || bearerToken !== process.env.AUTHORIZATION_TOKEN) {
-            return httpUnAuthorizedErrorMessage('Invalid authorization token');
-        }
-
-        const formData = (contentType.startsWith('multipart/form-data')) ? multipart.parse(event,true) : parse(event.body);
-
-        await sendBroadcastMessage(lineService,formData);
-        return httpOkMessage('Success Notify');
-    }
-
-    const body = JSON.parse(event.body);
-
-    /* health check from LINE */
-    if(body.events.length === 0) {
-        return httpOkMessage('No events');
-    }
-
-    /* reply default message */
-    await replyDefaultMessage(lineService, body);
-
-    return httpOkMessage('Success');
+    return await app.processRequest();
 };
---
+++ src/lineNotifyMessengerApp.ts
@@ -0,0 +1,62 @@
+import { ILineNotifyMessenger } from './interfaces/lineNotifyMessenger';
+import { IImageStorage } from './interfaces/imageStorage';
+import { IImageConverter } from './interfaces/imageConverter';
+import LineService from './lineService';
+
+export class LineNotifyMessengerApp {
+    private messenger: ILineNotifyMessenger;
+    private lineService: LineService;
+
+    constructor  (messenger:ILineNotifyMessenger, lineChannelAccessToken: string,imageStorage: IImageStorage, imageConverter: IImageConverter) {
+        this.messenger = messenger;
+        this.lineService = new LineService(lineChannelAccessToken, imageStorage, imageConverter);
+    }
+
+    private isNotifyServiceRequest = () => {
+        const path = this.messenger.getRequestPath();
+        const method = this.messenger.getHttpMethod();
+        const contentType = this.messenger.getContentType();
+
+        if(path === '/notify' && method === 'POST' && ( contentType === 'application/x-www-form-urlencoded' || contentType.startsWith('multipart/form-data'))) {
+            return true;
+        }
+        return false;
+    };
+    
+    private sendBroadcastMessage = async (formData: any) => {
+        await this.lineService.broadcastMessage(formData);
+    };
+    
+    private replyDefaultMessage = async (body: any) => {
+        const replyToken = body.events[0].replyToken;
+        await this.lineService.replyMessage(replyToken, 'お送り頂いたメッセージはどこにも送られないのでご注意ください');
+    };
+    
+    async processRequest() {
+        if(this.isNotifyServiceRequest()) {
+            const bearerToken = this.messenger.getBearerToken();
+    
+            if (!bearerToken || bearerToken !== process.env.AUTHORIZATION_TOKEN) {
+                return this.messenger.httpUnAuthorizedErrorMessage('Invalid authorization token');
+            }
+    
+            const formData = this.messenger.getFormData();
+    
+            await this.sendBroadcastMessage(formData);
+            return this.messenger.httpOkMessage('Success Notify');
+        }
+    
+        const body = JSON.parse(this.messenger.getBody());
+    
+        /* health check from LINE */
+        if(body.events.length === 0) {
+            return this.messenger.httpOkMessage('No events');
+        }
+    
+        /* reply default message */
+        await this.replyDefaultMessage(body);
+    
+        return this.messenger.httpOkMessage('Success');
+    };
+        
+}
