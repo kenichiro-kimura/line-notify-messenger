a0f75920f84757c906d7d18dd728f1bd5fe40edd
Kenichiro Kimura <39895593+kenichiro-kimura@users.noreply.github.com>
2025-02-25 20:51:13 +0900

    [add] functions用の実装を分離/追加

---
+++ src/functionsLineNotifyMessenger.ts
@@ -0,0 +1,77 @@
+import { ILineNotifyMessenger } from './interfaces/lineNotifyMessenger';
+
+export class FunctionsLineNotifyMessenger implements ILineNotifyMessenger {
+    private request: any;
+
+    constructor(request: any) {
+        this.request = request;
+    }
+
+    public httpInvalidRequestErrorMessage(message: string): any {
+        return {
+            status: 400,
+            body: message
+        };
+    };
+
+    public httpUnAuthorizedErrorMessage (message: string): any {
+        return {
+            status: 401,
+            body: message
+        };    
+    };
+
+    public httpInternalServerErrorMessage(message: string): any {
+        return {
+            status: 500,
+            body: message
+        };
+    };
+
+    public httpOkMessage(message: string): any {
+        return {
+            status: 200,
+            body: message
+        };
+    };
+
+    public getRequestPath(): string {
+        return this.request.url?.split('api/HttpTrigger')[1] || "";
+    }
+
+    public getHttpMethod(): string {
+        return this.request.method;
+    }
+
+    public getContentType(): string {
+        return this.request.headers.get('content-type') || this.request.headers.get('Content-Type') || "";
+    }
+
+    public getBearerToken(): string {
+        return this.request.headers.get('Authorization')?.split('Bearer ')[1] || "";
+    }
+
+    public async getFormDataAsync(): Promise<any> {
+        let formData: any = {};
+        if (this.getContentType().startsWith('multipart/form-data')) {
+            const rawFormData = await this.request.formData();
+            formData.message = rawFormData.get('message');
+            const imageFile : any = rawFormData.get('imageFile');
+            formData.imageFile = {
+                'filename': imageFile.name,
+                'contentType': imageFile.type,
+                'content': Buffer.from(imageFile.arrayBuffer())
+            }
+        } else {
+            let originalFormData = this.request.formData();
+            originalFormData.forEach((value, key) => {
+                formData[key] = value;
+            });
+        }
+        return formData;
+    }
+
+    public async getBodyAsync(): Promise<string> {
+        return await this.request.text();
+    }
+}
