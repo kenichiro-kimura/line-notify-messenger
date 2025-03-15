eac64b96381f30a19038c6b66f72e54dd4d83cc2
Kenichiro Kimura <39895593+kenichiro-kimura@users.noreply.github.com>
2025-03-15 11:23:16 +0900

    [modify] #23 ビジネスロジック実装

--- src/lineNotifyMessengerApp.ts
+++ src/lineNotifyMessengerApp.ts
@@ -3,13 +3,22 @@ import { FunctionsHttpResponse, ILineNotifyMessenger, LambdaHttpResponse } from 
 import { IImageStorage } from './interfaces/imageStorage';
 import { IImageConverter } from './interfaces/imageConverter';
 import LineService from './lineService';
+import { IGroupRepository } from './interfaces/groupRepository';
+
+enum SendMode {
+    broadcast = 'broadcast',
+    group = 'group',
+    all = 'all'
+}
 
 export class LineNotifyMessengerApp {
     private messenger: ILineNotifyMessenger;
     private lineService: LineService;
+    private groupRepository: IGroupRepository;
 
-    constructor  (messenger:ILineNotifyMessenger, lineChannelAccessToken: string,imageStorage: IImageStorage, imageConverter: IImageConverter) {
+    constructor  (messenger:ILineNotifyMessenger, lineChannelAccessToken: string,imageStorage: IImageStorage, imageConverter: IImageConverter, groupRepository: IGroupRepository) {
         this.messenger = messenger;
+        this.groupRepository = groupRepository;
         this.lineService = new LineService(lineChannelAccessToken, imageStorage, imageConverter);
     }
 
@@ -38,18 +47,74 @@ export class LineNotifyMessengerApp {
             return true;
         }
         return false;
-    };
+    }
     
+    private getRequestGroupId = (body: any) : string => {
+        return body.events[0].source?.groupId || "";
+    }
+
+    private addGroupId = async (groupId: string) => {
+
+        console.log(`Add group ${groupId}`);
+        return Promise.resolve("");
+    }
+
+    private getTargetGroupIds = async () : Promise<string[]> => {
+        return Promise.resolve([]);
+    }
+
+    private getSendMode = () : SendMode => {
+        const mode = process.env.SEND_MODE || SendMode.broadcast;
+        // process.env.SEND_MODEがSendModeに変換できない場合はSendMode.broadcastを返す
+        if (!Object.values(SendMode).includes(mode as SendMode)) {
+            return SendMode.broadcast;
+        } else {
+            return mode as SendMode;
+        } 
+    }
+
     private sendBroadcastMessage = async (formData: any) => {
         await this.lineService.broadcastMessage(formData);
-    };
+    }
     
+    private sendGroupMessage = async (formData: any) => {
+        const groupIds : string [] = await this.getTargetGroupIds();
+        if(groupIds.length > 0) {
+            await this.lineService.sendGroupMessage(groupIds, formData);
+        }
+    }
+
+    private sendMessage = async (FormData: any, sendMode: SendMode) => {
+        switch (sendMode) {
+            case SendMode.broadcast:
+                await this.sendBroadcastMessage(FormData);
+                break;
+            case SendMode.group:
+                await this.sendGroupMessage(FormData);
+                break;
+            case SendMode.all:
+                await this.sendBroadcastMessage(FormData);
+                await this.sendGroupMessage(FormData);
+                break;
+        }
+    }
+
     private replyDefaultMessage = async (body: any) => {
         const replyToken = body.events[0].replyToken;
         await this.lineService.replyMessage(replyToken, 'お送り頂いたメッセージはどこにも送られないのでご注意ください');
-    };
+    }
     
     async processRequest(): Promise<LambdaHttpResponse | FunctionsHttpResponse> {
+        const body = JSON.parse(await this.messenger.getHttpBodyAsync());    
+        const sendMode = this.getSendMode();
+        const groupId : string = this.getRequestGroupId(body);
+
+        if(groupId !== "") {
+            // リクエストにグループIDが含まれている場合、グループIDをデータストアに追加する
+            await this.addGroupId(groupId);
+            return this.httpOkMessage('Success Add Group');
+        }
+
         if(this.isNotifyServiceRequest()) {
             const bearerToken = this.getBearerToken();
     
@@ -59,11 +124,9 @@ export class LineNotifyMessengerApp {
     
             const formData = await this.messenger.getHttpFormDataAsync();
     
-            await this.sendBroadcastMessage(formData);
+            await this.sendMessage(formData,sendMode);
             return this.httpOkMessage('Success Notify');
         }
-    
-        const body = JSON.parse(await this.messenger.getHttpBodyAsync());
     
         /* health check from LINE */
         if(body.events.length === 0) {
